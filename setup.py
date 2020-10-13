ce8fdc1db59e3a817818069c0d88e406782c3fb1
namlehong <namlehong@gmail.com>
2020-10-14 02:11:12 +0700

    release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="cc-captcha-solver",  # Replace with your own username
+    name="cc-captcha",  # Replace with your own username
     version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
